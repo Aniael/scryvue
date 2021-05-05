3afa3275d2c70105c6abcee3d6eaac47b0e1cec5
Aniael <Theimpossibleoficcial@gmail.com>
2021-05-05 06:19:46 -0500

    commit

--- ScryWeb/src/components/Home.vue
+++ ScryWeb/src/components/Home.vue
@@ -2,10 +2,27 @@
 <div class="container-fluid m-0 p-0">
 
     <div id="main" class="full-height bg-danger p-4">
+      <div class="container pt-3">
+
+        <div class="row pb-5">
+          <div class="col-lg-3">
+          </div>
+
+          <div class="col-lg-6">
+            <img id="logo" src="../assets/lclogo.png" class="img-fluig">
+          </div>
+
+          <div class="col-lg-3">
+
+          </div>
+
+          </div>
+
+      </div>
 
     </div>
 
-    <div id="row2" class="bg-dark p-4">
+    <div id="row2" class="bg-dark p-3">
 
     </div>
 
@@ -29,5 +46,9 @@ export default {
     -moz-background-size: cover;
     -o-background-size: cover;
     background-size: cover;
+}
+
+#logo {
+  max-height: 50vh;
 }
 </style>
